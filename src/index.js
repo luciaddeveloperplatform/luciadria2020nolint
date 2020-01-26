ed286eb7c4e0f0480c2326975ffe6b11a21c9dfa
luciaddevelopmentplatform <60262140+luciaddevelopmentplatform@users.noreply.github.com>
2020-01-26 08:41:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+// Import LuciadRIA libraries
 import { WebGLMap } from "@luciad/ria/view/WebGLMap";
 import { getReference } from '@luciad/ria/reference/ReferenceProvider';
 import { createBounds } from "@luciad/ria/shape/ShapeFactory";
 
+// Import local style file
 import "./index.scss";
 
+// Get the html element with root id
 const root = document.getElementById("root");
 
+// Create a HTML element to insert our map
 const mapElement = document.createElement("div");
+// Assign a class to style it with css
 mapElement.classList.add("LuciadMap");
+
+// Insert the mapElement
 root.appendChild(mapElement);
 
+// Create the map and fit to bounds
 const map = new WebGLMap(mapElement, {reference: getReference("EPSG:4978")});
 map.mapNavigator.fit({bounds: createBounds(getReference("CRS:84"), [-122, 60, 25, 30])});
