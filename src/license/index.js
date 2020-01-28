303f03285386071743a7836df522af0ccbb9e48a
luciaddevelopmentplatform <60262140+luciaddevelopmentplatform@users.noreply.github.com>
2020-01-28 14:38:06 +0100

    Update index.js

--- src/license/index.js
+++ src/license/index.js
@@ -1,4 +1,11 @@
-import {setLicenseText} from "@luciad/ria/util/License";
+import { setLicenseText } from "@luciad/ria/util/License";
 
-const licenseText = require('!!raw-loader!./luciadria_development.txt').default;
-setLicenseText(licenseText);
+const licenseText = require("!!url-loader!./luciadria_development.txt").default;
+
+function decodeBase64StringToPlainText(licenseBase64) {
+  // Base64 String has the format: data:text/plain;base64,code
+  const parts = licenseBase64.split(",");
+  return parts.length === 2 ? atob(parts[1]) : "";
+}
+
+setLicenseText(decodeBase64StringToPlainText(licenseText));
